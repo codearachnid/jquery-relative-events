56ddba5db4afce4b747bc456747bdf145a652d74
Timothy Wood <tim@imaginesimplicity.com>
2013-11-15 11:37:01 -0500

    added multiday demo events

--- jquery.relativeEvents.demo.js
+++ jquery.relativeEvents.demo.js
@@ -1,14 +1,14 @@
 /**
  * DEMO EVENTS
  */
-var demo_events = [
-	// 3x1 events
-	{ hour: "7", min: "00", duration: "30" },
-	{ hour: "7", min: "00", duration: "30" },
-	{ hour: "7", min: "15", duration: "30" },
-	// 1x1 event
-	{ hour: "7", min: "45", duration: "45" },
-	// 2x1 events
+var single_day = [
+    // 3x1 events
+    { hour: "7", min: "00", duration: "30" },
+    { hour: "7", min: "00", duration: "30" },
+    { hour: "7", min: "15", duration: "30" },
+    // 1x1 event
+    { hour: "7", min: "45", duration: "45" },
+    // 2x1 events
     { hour: "8", min: "30", duration: "30" },
     { hour: "8", min: "30", duration: "30" },
     // 7x2.5 events
@@ -24,7 +24,6 @@ var demo_events = [
     { hour: "10", min: "00", duration: "30" },
     { hour: "10", min: "00", duration: "30" },
     { hour: "10", min: "00", duration: "30" },
-    // { hour: "10", min: "00", duration: "30" },
     // 4x1 event
     { hour: "11", min: "00", duration: "30" },
     { hour: "11", min: "00", duration: "30" },
@@ -49,31 +48,95 @@ var demo_events = [
     { hour: "15", min: "00", duration: "60" },
     { hour: "15", min: "15", duration: "60" },
 ];
+var full_week = [
+{ date: '2013-11-10', events: [
+    { hour: "11", min: "00", duration: "30" },
+    { hour: "11", min: "00", duration: "30" },
+    { hour: "12", min: "00", duration: "30" },
+    { hour: "13", min: "00", duration: "30" },
+]},
+{ date: '2013-11-11', events: [
+    { hour: "9", min: "00", duration: "105" },
+    { hour: "9", min: "00", duration: "30" },
+    { hour: "9", min: "00", duration: "30" },
+    { hour: "10", min: "00", duration: "60" },
+    { hour: "10", min: "00", duration: "30" },
+]},
+{ date: '2013-11-12', events: [
+	{ hour: "7", min: "00", duration: "30" },
+	{ hour: "7", min: "00", duration: "30" },
+	{ hour: "7", min: "15", duration: "30" },
+	{ hour: "7", min: "45", duration: "45" },
+    { hour: "8", min: "30", duration: "30" },
+    { hour: "8", min: "30", duration: "30" },
+]},
+{ date: '2013-11-13', events: [
+    { hour: "11", min: "00", duration: "30" },
+    { hour: "11", min: "00", duration: "30" },
+    { hour: "11", min: "00", duration: "30" },
+    { hour: "11", min: "00", duration: "30" },
+]},
+{ date: '2013-11-14', events: [
+    { hour: "7", min: "45", duration: "45" },
+    { hour: "8", min: "30", duration: "120" },
+    { hour: "8", min: "30", duration: "90" },
+]},
+{ date: '2013-11-15', events: []},
+{ date: '2013-11-16', events: [
+    { hour: "7", min: "45", duration: "300" },
+]}];
 /**
  * EVENT OVERLAP DEMO
  */
 jQuery(document).ready(function($){
+
     // DUMMY DATA
-    // loop through dummy event data and create the divs
-    $.each(demo_events, function(i,item){
-        // create event divs
-        $('#demoEvents').append($('<div>').attr( {
-            class: 'event',
-            id: 'event-' + i,
-            'data-hour':item.hour,
-            'data-minute': item.min,
-            'data-duration':item.duration
-        }).text( "Event " + (i+1) ));
-        if( demo_events.length-1 == i ){
-            $('.calendar').relativeEvents({
-            	dayStart: 7,
-                eventOffset: {
-                    top: -52, // offset because of 48 borders for hour and half hour dividers
-                    left: 64,
-                    height: -5, // offset height because of the event borders
-                    width: -5, // offset width because of the event borders
-                },
-                debug: true
+    // loop through single day event data
+    $.each(single_day, function(i,item){
+        // create event divs for single day
+        $('#single_day .day').append($('<div>').attr( {
+                class: 'event',
+                id: 'event-' + i,
+                'data-hour':item.hour,
+                'data-minute': item.min,
+                'data-duration':item.duration
+            }).text( "Event " + (i+1) ));
+        if( single_day.length-1 == i ){
+
+            // loop through full week event data
+            $.each(full_week, function(index, column){
+                
+                // create daily event columns
+                // <div class="events day" data-day="2013-11-11"></div> 
+                $('#full_week').append( $('<div>').attr({
+                    class: 'events day',
+                    'data-date': column.date
+                }));
+            
+                $.each(column.events, function(ii,item){
+                    // create event divs
+                    $('#full_week').find('.day[data-date=' +column.date+ ']').append($('<div>').attr( {
+                            class: 'event',
+                            id: 'event-' + ii,
+                            'data-hour':item.hour,
+                            'data-minute': item.min,
+                            'data-duration':item.duration
+                        }).text( "Event " + (ii+1) ));
+                });
+
+                // once done with full week fire off relativeEvents
+                if( full_week.length-1 == index ){
+                    $('.calendar').relativeEvents({
+                        dayStart: 7,
+                        eventOffset: {
+                            top: -61, // offset because of 48 borders for hour and half hour dividers
+                            left: 5,
+                            height: -4, // offset height because of the event borders
+                            width: -6, // offset width because of the event borders
+                        },
+                        debug: true
+                    });
+                }
             });
         }
     });
