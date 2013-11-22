d91e3a33ce50a70b0f0626cbe9d2eddf0951c86a
Timothy Wood <tim@imaginesimplicity.com>
2013-11-21 19:54:15 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -131,7 +131,7 @@ jQuery(document).ready(function($){
                         debug: true,
                         eventOffset: {
                 top: -61, // offset because of 48 borders for hour and half hour dividers
-                left: 10,
+                left: 6.5,
                 height: -5, // offset height because of the event borders
                 width: -6, // offset width because of the event borders
             }
